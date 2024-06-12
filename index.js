8a384ad8491f8a3544c5a0519ac952d978edba67
sadmanalam142 <sadmanalam142@gmail.com>
2024-06-12 15:42:48 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,80 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const jwt = require("jsonwebtoken");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const cors = require("cors");
+const port = process.env.PORT;
+
+app.use(cors());
+app.use(express.json());
+
+function createToken(user) {
+  const token = jwt.sign(
+    {
+      email: user.email,
+    },
+    "secret",
+    { expiresIn: "7d" }
+  );
+  return token;
+}
+
+function verifyToken(req, res, next) {
+  const token = req.headers.authorization.split(" ")[1];
+  const verify = jwt.verify(token, "secret");
+  if (!verify?.email) {
+    return res.send("You are not authorized");
+  }
+  req.user = verify.email;
+  next();
+}
+
+const uri = process.env.DATABASE_URL;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const ticket_booking_DB = client.db("ticket_booking_DB");
+    const userCollection = ticket_booking_DB.collection("userCollection");
+
+   
+    
+    // user
+    app.post("/user", async (req, res) => {
+      const user = req.body;
+
+      const token = createToken(user);
+      const isUserExist = await userCollection.findOne({ email: user?.email });
+      if (isUserExist?._id) {
+        return res.send({
+          statu: "success",
+          message: "Login success",
+          token,
+        });
+      }
+      await userCollection.insertOne(user);
+      return res.send({ token });
+    });
+
+    console.log("Database is connected");
+  } finally {
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Route is working");
+});
+
+app.listen(port, (req, res) => {
+  console.log("App is listening on port :", port);
+});
