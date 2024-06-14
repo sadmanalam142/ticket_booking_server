c3ee06d4b61307a3b06cc500fb73e212b17e280e
sadmanalam142 <sadmanalam142@gmail.com>
2024-06-14 15:27:16 +0600

    payment route added

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express();
 const jwt = require("jsonwebtoken");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const cors = require("cors");
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 const port = process.env.PORT;
 
 app.use(cors());
@@ -47,36 +48,50 @@ async function run() {
     const ticketCollection = ticket_booking_DB.collection("ticketCollection");
     const bookingCollection = ticket_booking_DB.collection("bookingCollection");
     const userCollection = ticket_booking_DB.collection("userCollection");
+    const paymentCollection = ticket_booking_DB.collection("paymentCollection");
 
-   //ticket
-   app.get("/tickets", async (req, res) => {
-    const ticketsData = ticketCollection.find();
-    const result = await ticketsData.toArray();
-    res.send(result);
-  });
-
-  app.get("/tickets/:id", async (req, res) => {
-    const id = req.params.id;
-    const ticketsData = await ticketCollection.findOne({
-      _id: new ObjectId(id),
+    //ticket
+    app.get("/tickets", async (req, res) => {
+      const ticketsData = ticketCollection.find();
+      const result = await ticketsData.toArray();
+      res.send(result);
     });
-    res.send(ticketsData);
-  });
 
-  //booking
-  app.post("/booking", verifyToken, async (req, res) => {
-    const bookingData = req.body;
-    const result = await bookingCollection.insertOne(bookingData);
-    res.send(result);
-  });
+    app.get("/tickets/:id", async (req, res) => {
+      const id = req.params.id;
+      const ticketsData = await ticketCollection.findOne({
+        _id: new ObjectId(id),
+      });
+      res.send(ticketsData);
+    });
 
-  app.get("/booking/:email", async (req, res) => {
-    const email = req.params.email;
-    const query = { email: email };
-    const result = await bookingCollection.find(query).toArray();
-    res.send(result);
-  });
-    
+    //booking
+    app.post("/booking", verifyToken, async (req, res) => {
+      const bookingData = req.body;
+      const result = await bookingCollection.insertOne(bookingData);
+      res.send(result);
+    });
+
+    app.get("/bookings/:id", verifyToken,  async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await bookingCollection.findOne(query);
+      res.send(result);
+    });
+
+    app.get("/booking/:email", verifyToken, async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const result = await bookingCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    app.delete("/booking/:id", verifyToken, async (req, res) => {
+      const id = req.params.id;
+      const result = await bookingCollection.deleteOne({ _id: new ObjectId(id) });
+      res.send(result);
+    });
+
     // user
     app.post("/user", async (req, res) => {
       const user = req.body;
@@ -92,6 +107,63 @@ async function run() {
       }
       await userCollection.insertOne(user);
       return res.send({ token });
+    });
+
+    app.get("/user/get/:id", async (req, res) => {
+      const id = req.params.id;
+      const result = await userCollection.findOne({ _id: new ObjectId(id) });
+      res.send(result);
+    });
+
+    app.get("/user/:email", async (req, res) => {
+      const email = req.params.email;
+      const result = await userCollection.findOne({ email });
+      res.send(result);
+    });
+
+    app.patch("/user/:email", async (req, res) => {
+      const email = req.params.email;
+      const userData = req.body;
+      const result = await userCollection.updateOne(
+        { email },
+        { $set: userData },
+        { upsert: true }
+      );
+      res.send(result);
+    });
+
+    //payment
+    app.post("/create-payment-intent", async (req, res) => {
+      const booking = req.body;
+      const price = booking.price;
+      const amount = price * 100;
+
+      const paymentIntent = await stripe.paymentIntents.create({
+        currency: "usd",
+        amount: amount,
+        payment_method_types: ["card"],
+      });
+      res.send({
+        clientSecret: paymentIntent.client_secret,
+      });
+    });
+
+    app.post("/payments", async (req, res) => {
+      const payment = req.body;
+      const result = await paymentCollection.insertOne(payment);
+      const id = payment.bookingId;
+      const filter = { _id: new ObjectId(id) };
+      const updatedDoc = {
+        $set: {
+          paid: true,
+          transactionId: payment.transactionId,
+        },
+      };
+      const updatedResult = await bookingCollection.updateOne(
+        filter,
+        updatedDoc
+      );
+      res.send(result);
     });
 
     console.log("Database is connected");
